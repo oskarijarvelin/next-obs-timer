8c20bcb2a7fce648c21bd639bbb5a0563874aee8
Oskari Järvelin <contact@oskarijarvelin.fi>
2022-05-30 01:31:47 +0300

    Updated countdown

--- pages/countdown.js
+++ pages/countdown.js
@@ -1,15 +1,23 @@
+import React, { useState, useEffect } from 'react'
 import Moment from 'react-moment'
 import moment from 'moment'
 import styles from '../index.module.css'
 
 export default function Countdown({time}) {
     const secs = moment(time, 'HH:mm:ss').diff(moment(), 'seconds');
-    const formatted = moment.utc(secs*1000).format('HH:mm:ss');
-    const target = moment( moment().format('DD.MM.YYYY ') + time, 'DD.MM.YYYY HH:mm:ss');
+    const [hour, setHour] = useState(moment.utc(secs*1000).format('HH:mm:ss'));
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setHour(moment.utc(secs*1000).format('HH:mm:ss'));
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, [secs]);
 
     return (
         <div suppressHydrationWarning className={styles.container}>
-            {formatted}
+            {hour}
         </div>
     )
 }
