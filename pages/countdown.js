31ed0db7c8fd5f83f55ac3d4b12b09e1cafa2229
Oskari Järvelin <contact@oskarijarvelin.fi>
2022-05-30 01:45:29 +0300

    Finished countdown

--- pages/countdown.js
+++ pages/countdown.js
@@ -1,26 +1,41 @@
-import React, { useState, useEffect } from 'react'
-import Moment from 'react-moment'
+import React, {useEffect, useMemo, useState} from 'react'
 import moment from 'moment'
 import styles from '../index.module.css'
 
-export default function Countdown({time}) {
-    const secs = moment(time, 'HH:mm:ss').diff(moment(), 'seconds');
-    const [hour, setHour] = useState(moment.utc(secs*1000).format('HH:mm:ss'));
+export default function Countdown({ time }) {
+    const timeInSecs = moment(time, 'HH:mm:ss').diff(moment(), 'seconds');
+    const [timeLeft, setTimeLeft] = useState(timeInSecs);
+
+    const remainTime = useMemo(() => {
+        const days = Math.floor(timeLeft / 24 / 3600);
+        const hours = Math.floor((timeLeft - days * 24 * 3600) / 3600);
+        const minutes = Math.floor((timeLeft - days * 24 * 3600 - hours * 3600) / 60);
+        const seconds = (timeLeft - days * 24 * 3600 - hours * 3600) % 60;
+
+        return {
+            days,
+            hours,
+            minutes,
+            seconds
+        }
+    }, [timeLeft]);
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setHour(moment.utc(secs*1000).format('HH:mm:ss'));
+            setTimeLeft(timeLeft => timeLeft !== 0 ? timeLeft - 1 : 0);
         }, 1000);
 
         return () => clearInterval(interval);
-    }, [secs]);
+    }, []);
 
     return (
         <div suppressHydrationWarning className={styles.container}>
-            {hour}
+            {String(remainTime.hours).padStart(2, '0')}:{String(remainTime.minutes).padStart(2, '0')}:{String(remainTime.seconds).padStart(2, '0')}
         </div>
-    )
-}
+    );
+};
+
+;
 
 Countdown.getInitialProps = async ({ query }) => {
     const {time} = query
